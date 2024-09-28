be68bd95fb16c176ffe67eaacbaf57008b7a9d65
Anish Neupane <anishneupane2058@gmail.com>
2024-09-28 09:31:31 +0545

    Added Candidate entity and Email value object.

---
+++ Domain/Entities/Candidate.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Candidate
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Email Email { get; set; }
+        public DateTime PreferredCallTime { get; set; }
+        public string LinkedInProfileUrl { get; set; }
+        public string GitHubProfileUrl { get; set; }
+        public string Comment { get; set; }
+
+
+    }
+}
