103fac8a539290ee4eb9bd52f97ef6cc8890bb9e
Anish Neupane <anishneupane2058@gmail.com>
2024-09-28 12:09:58 +0545

    Candidate Controller added.

---
+++ WebUI/Controllers/CandidateController.cs
@@ -0,0 +1,30 @@
+﻿using Application.CandidateInfos.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using W;
+
+namespace WebUI.Controllers
+{
+    public class CandidateController : ApiControllerBase
+    {
+        [HttpPost]
+        [Route("addOrUpdate")]
+        public async Task<ActionResult<Guid>> AddOrUpdateCandidate(AddorUpdateCandidateCommand command)
+        {
+            Guid result = await Mediator.Send(command);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("view")]
+        public async Task<ActionResult<Guid>> GetAllCandidate(AddorUpdateCandidateCommand command)
+        {
+            Guid result = await Mediator.Send(command);
+
+            return Ok(result);
+        }
+
+    }
+}
