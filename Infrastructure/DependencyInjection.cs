1837d4c4ba2c4106e4cbbd2298984ef35fdf89a5
Anish Neupane <anishneupane2058@gmail.com>
2024-09-29 21:26:48 +0545

    Inmemory database implemented.

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -10,33 +10,11 @@ namespace Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
-            {
-                services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("CleanArchitectureDb"));
-            }
-            else
-            {
-                if (configuration.GetValue<bool>("UseSQLite"))
-                {
-                    services.AddDbContext<ApplicationDbContext>(options =>
-                    {
-                        options.ConfigureWarnings(options => options.Ignore(RelationalEventId.AmbientTransactionWarning));
-                        options.UseSqlite(@"Data Source=E:\ICMS.db", sqlLiteOptions => sqlLiteOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
-                    });
-                }
-                else
-                {
-                    services.AddDbContext<ApplicationDbContext>(options =>
-                    {
-                        options.EnableSensitiveDataLogging();
-                        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
-                                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
-                    });
-
-                }
-            }
+            services.AddDbContext<ApplicationDbContext>(options =>
+                                                            options.UseInMemoryDatabase("CandidateDb"));
 
 
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
 
